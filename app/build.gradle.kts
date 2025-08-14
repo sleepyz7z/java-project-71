c70cd71ca1ff8222815ff8cebb7aface627608b6
Igor Kushvid <kushvidis@mail.ru>
2025-08-14 05:20:04 -0700

    3 step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true,
+        version = "1.0"
+)
+public class App implements Runnable {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
     }
+
+    @Override
+    public void run() {}
 }
