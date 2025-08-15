f10f5da5033616f939463b1a6d381b9085eef54d
Igor Kushvid <kushvidis@mail.ru>
2025-08-15 01:04:45 -0700

    4 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 @Command(
         name = "gendiff",
@@ -9,11 +11,41 @@ import picocli.CommandLine.Command;
         mixinStandardHelpOptions = true,
         version = "1.0"
 )
+
 public class App implements Runnable {
+    @Parameters(
+            index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1"
+    )
+    private String filepath1;
+
+    @Parameters(
+            index = "1",
+            description = "path to secomd file",
+            paramLabel = "filepath2"
+    )
+    private String filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish",
+            paramLabel = "format"
+    )
+    private String format;
+
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
     }
 
     @Override
-    public void run() {}
+    public void run() {
+        try {
+            String diff = Differ.generate(filepath1, filepath2);
+            System.out.println(diff);
+        } catch (Exception e) {
+            System.err.println("Error comparing files: " + e.getMessage());
+        }
+    }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+
+public class Differ {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        // Временная заглушка - позже замените реальной логикой
+        return "File comparison result will be here\n"
+                + "File 1: " + filePath1 + "\n"
+                + "File 2: " + filePath2;
+    }
+}
